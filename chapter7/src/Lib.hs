7ed3a47204a352d5026cb235b6fa7495009316a1
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2020-09-09 23:14:16 +0530

    List monads from exercise 7-1

--- chapter7/src/Lib.hs
+++ chapter7/src/Lib.hs
@@ -1,6 +1,15 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Control.Monad
+
+jumps = [-1, 3, 5]
+
+brokenThreeJumps :: [Int]
+brokenThreeJumps = do
+  j1 <- jumps
+  j2 <- jumps
+  j3 <- jumps
+  return $ j1 + j2 + j3
+
+brokenJumps :: Int -> [Int]
+brokenJumps n = map sum $ replicateM n jumps
--- chapter7/test/Spec.hs
+++ chapter7/test/Spec.hs
@@ -1,2 +1,12 @@
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain testCases
+
+testCases :: TestTree
+testCases = testGroup "Tests" [brokenJumpTest]
+
+brokenJumpTest = testCase "broken jumps should be the same" $ do
+  (brokenJumps 3) @?= brokenThreeJumps
