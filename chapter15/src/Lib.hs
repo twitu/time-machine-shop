3f26a35f2cd87bf2fefff6406510facee6e7d523
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2020-12-09 07:22:46 +0530

    Configure testing for binary tree

--- chapter15/src/Lib.hs
+++ chapter15/src/Lib.hs
@@ -1,6 +1,62 @@
+-- | Simple implementation of binary trees
 module Lib
-    ( someFunc
-    ) where
+  (
+  -- * The main data type
+    BinaryTree(..)
+  ,
+  -- * Operations
+  -- ** Insertion
+    treeInsert
+  , treeMerge
+  ,
+  -- ** Lookup
+    treeFind
+  , treeLen
+  ,
+  -- ** Removal
+    treeDelete
+  )
+where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- | A typical binary tree
+data BinaryTree a = Node a (BinaryTree a) (BinaryTree a) -- ^ Inner nodes
+                  | Leaf -- ^ Leaves
+                  deriving (Show, Eq)
+
+{-|
+Inserts an element into a 'BinaryTree'
+
+  * If it finds a leaf, insert there
+
+  * If smaller than the item in the node, insert in the left
+
+  * If larger than the item in the node, insert in the right
+
+>>> treeInsert 1 Leaf
+Node 1 Leaf Leaf
+-}
+treeInsert :: Ord a => a -> BinaryTree a -> BinaryTree a
+treeInsert x Leaf = Node x Leaf Leaf
+treeInsert x (Node y l r) | x <= y    = Node y (treeInsert x l) r
+                          | otherwise = Node y l (treeInsert x r)
+
+treeMerge :: Ord a => BinaryTree a -> BinaryTree a -> BinaryTree a
+treeMerge t Leaf         = t
+treeMerge t (Node x l r) = treeInsert x $ treeMerge (treeMerge t l) r
+
+treeLen :: BinaryTree a -> Integer
+treeLen Leaf         = 0
+treeLen (Node _ l r) = 1 + treeLen l + treeLen r
+
+treeFind :: Ord a => a -> BinaryTree a -> Maybe a
+treeFind t (Node v l r) = case compare t v of
+  EQ -> Just v
+  LT -> treeFind t l
+  GT -> treeFind t r
+treeFind _ Leaf = Nothing
+
+treeDelete :: Ord a => a -> BinaryTree a -> BinaryTree a
+treeDelete _ Leaf = Leaf
+treeDelete x (Node y l r) | x == y = Leaf
+                          | x > y  = Node y (treeDelete x l) r
+                          | x < y  = Node y l (treeDelete x r)
--- chapter15/test/Spec.hs
+++ chapter15/test/Spec.hs
@@ -1,2 +1,18 @@
+module Main where
+
+import           Test.Tasty
+import           Test.Tasty.HUnit              as HU
+import           Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain allTests
+
+allTests :: TestTree
+allTests =
+  testGroup "Tasty Tests" [testGroup "HUnit Tests" [hunitTestInsertOnLeaf]]
+
+hunitTestInsertOnLeaf :: TestTree
+hunitTestInsertOnLeaf = HU.testCase "Insert 'a' on empty tree" $ assertEqual
+  "Insertion is wrong"
+  (treeInsert 'a' Leaf)
+  (Node 'a' Leaf Leaf)
